06bf0a2a070e46d38d4935aa947d2fa4b0907259
Christopher Sexton <cpsexton@gmail.com>
2019-12-21 19:11:35 -0500

    adds getguess function and refactors

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -5,22 +5,42 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+
 int main() {
-	constexpr int WORD_LENGTH = 9;
+	PrintIntro();
+	GetGuess();
+}
+
 	// introduce the game
+void PrintIntro() {
+	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bull and Cows!\n";
+	cout << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+	cout << "You get 5 attempts.\n";
 	cout << endl;
+	return;
+}
 	
 	// get a guess from the player
+string GetGuess() {
 	string Guess = "";
-	cout << "Enter your guess: " << Guess;
-	cin >> Guess;
-	cout << "You have entered " << Guess;
-	
 
-	//repeat the guess back to the player
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		cout << endl;
+		cout << "Enter your guess: ";
+		getline(cin, Guess);
+		cout << endl;
 
-	return 0;
+		//repeat the guess back to the player
+		cout << "You have entered: " << Guess << endl;
+		cout << endl;
+		cout << "You have used " << count << " attempts.";
+		cout << endl;
+	}
+	return Guess;
 }
