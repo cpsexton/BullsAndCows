716eb1212cf76ad43419ba89fba6549e027703b1
Christopher Sexton <cpsexton@gmail.com>
2019-12-21 21:11:26 -0500

    adds playgame function and refactors

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,4 +1,4 @@
-//main file for BullCowGame
+//main file for Bulls and Cows Game
 
 #include <iostream>
 #include <string>
@@ -6,41 +6,49 @@
 using namespace std;
 
 void PrintIntro();
+void PlayGame();
 string GetGuess();
 
 int main() {
 	PrintIntro();
-	GetGuess();
+	PlayGame();
+	
+	return 0; //exit the game
 }
 
-	// introduce the game
+
+void PlayGame() {
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		GetGuess();
+	};
+}
+
+	//introduce the game
 void PrintIntro() {
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bull and Cows!\n";
 	cout << endl;
-	cout << "Can you guess the " << WORD_LENGTH;
+	cout << "Can you guess the " << WORD_LENGTH; 
 	cout << " letter isogram I'm thinking of?\n";
 	cout << "You get 5 attempts.\n";
 	cout << endl;
 	return;
 }
 	
-	// get a guess from the player
+	//get a guess from the player
 string GetGuess() {
 	string Guess = "";
+	cout << endl;
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+	cout << endl;
 
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
-		cout << endl;
-		cout << "Enter your guess: ";
-		getline(cin, Guess);
-		cout << endl;
-
-		//repeat the guess back to the player
-		cout << "You have entered: " << Guess << endl;
-		cout << endl;
-		cout << "You have used " << count << " attempts.";
-		cout << endl;
-	}
+	//repeat the guess back to the player
+	cout << "You have entered: " << Guess << endl;
+	cout << endl;
+	cout << "You have used " << count << " attempts."; //this is broken. count needs to be a var of number of attempts taken
+	cout << endl;
+	
 	return Guess;
 }
